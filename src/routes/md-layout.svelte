46afbecb560e44e063d8e50659a60bb96a364d21
hazylol <hazycora+lolgithub@gmail.com>
2023-07-03 11:37:07 -0500

    add meta tags

--- src/routes/md-layout.svelte
+++ src/routes/md-layout.svelte
@@ -5,10 +5,30 @@
 
 <script>
 	export let title
+	export let description = 'A new, community-focused Minecraft server.'
+	export let image
 </script>
 
 <svelte:head>
 	<title>{title}</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://mangomc.net/" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+
+	<!-- Twitter -->
+	<meta property="twitter:url" content="https://mangomc.net/" />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	{#if image}
+		<meta property="og:image" content={image} />
+		<meta property="twitter:card" content="summary_large_image" />
+		<meta property="twitter:image" content={image} />
+	{/if}
 </svelte:head>
 
 <article>
